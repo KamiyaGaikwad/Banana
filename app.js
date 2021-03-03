bbe544eb9b79a33c7d84c087980353f9c08c5edb
Kamiya Gaikwad <kamitarg88@gmail.com>
2021-03-04 00:04:19 +0530

    uptoexercise11

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 var btn1 = document.querySelector("#btnn"); 
 var text = document.querySelector("#t1");
 var outp = document.querySelector("#output");
-function clickedbtn() {
-    console.log("clicked");
-    console.log("input",text.value);
-    outp.innerText = "output " + text.value;
-    var x = outp.innerText
-    console.log(x)
 
+//var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function constructurl(intput){
+    return serverURL + "?text=" + text.value;
+};
+
+function errorhandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server! Please try again");
+};
+
+function clickedbtn(){
+    var intput = text.value;
+    fetch(constructurl(intput))
+    .then(response => response.json())
+    .then(json => {
+        var translated1 = json.contents.translated;
+        outp.innerText = translated1;
+    
+    })
+    .catch(errorhandler)
     };
-
 btn1.addEventListener("click", clickedbtn)
