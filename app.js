cc70e59fab60136c45e93027ca1bcc6711f479c4
Kamiya Gaikwad <kamitarg88@gmail.com>
2021-03-03 19:33:13 +0530

    Exercise4

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var btn1 = document.querySelector("#btnn"); 
+var text = document.querySelector("#t1");
+console.log(text);
 console.log("This script is working");
-    
+   
 function clickedbtn() {
     console.log("clicked");
+    console.log("input",text.value);
     };
 
 btn1.addEventListener("click", clickedbtn)
